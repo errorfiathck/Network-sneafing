9a31ef60b188f9c249195905f26f1db0c09f462b
error <110461609+errorfiathck@users.noreply.github.com>
2023-04-25 13:02:54 +0330

    Add files via upload

---
+++ Network-sneafing/sneaf.py
@@ -0,0 +1,23 @@
+import socket 
+import os
+
+HOST = input("Enter your HOST ===> ")
+def main():
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    print(sniffer.recvfrom(65565))
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+    main()
